329a0914606cb422a5a118c8164a87b349ac0cf2
Dilan Morar <dilanmorar@gmail.com>
2020-11-10 11:09:05 +0000

    making the output more user friendly

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def db_version():
     cursor = db.cursor()
     cursor.execute("select version()")
     version = cursor.fetchone()
-    print(version)
-    close_db()
+    print('MySQL:', version)
+    close_db(db)
 
 # getting hospital info from hospital id
 def hospital_info(hospital_id):
@@ -33,8 +33,10 @@ def hospital_info(hospital_id):
     cursor.execute("""select * from hospital where hospital_id = %s""", (hospital_id,))
     h_info = cursor.fetchall()
     for detail in h_info:
-        print(detail[0], detail[1], detail[2])
-    close_db()
+        print('Hospital ID:', detail[0])
+        print('Hospital Name:', detail[1])
+        print('Bed Count:', detail[2])
+    close_db(db)
 
 # getting doctor info from doctor id
 def doctor_info(doctor_id):
@@ -43,16 +45,21 @@ def doctor_info(doctor_id):
     cursor.execute("""select * from doctor where doctor_id = %s""", (doctor_id,))
     d_info = cursor.fetchall()
     for detail in d_info:
-        print(detail[0], detail[1], detail[2], detail[3], detail[4], detail[5], detail[6])
-    close_db()
+        print('Doctor ID:', detail[0])
+        print('Doctor Name:', detail[1])
+        print('Hospital ID:', detail[2])
+        print('Joining Date:', detail[3])
+        print('Speciality:', detail[4])
+        print('Salary:', detail[5])
+        print('Experience:', detail[6])
+    close_db(db)
 
-# updating doctor speciality
 def update_doctor_speciality(speciality, doctor_id):
     db = get_db()
     cursor = db.cursor()
     cursor.execute("""update doctor set speciality = %s where doctor_id = %s """, (speciality, doctor_id))
     db.commit()
-    close_db()
+    close_db(db)
 
 # getting doctor from speciality and salary
 def doctor_speciality_salary(speciality, salary):
@@ -61,8 +68,14 @@ def doctor_speciality_salary(speciality, salary):
     cursor.execute("""select * from doctor where speciality = %s and salary > %s""", (speciality, salary))
     d_speciality_salary = cursor.fetchall()
     for detail in d_speciality_salary:
-        print(detail[0], detail[1], detail[2], detail[3], detail[4], detail[5], detail[6])
-    close_db()
+        print('Doctor ID:', detail[0])
+        print('Doctor Name:', detail[1])
+        print('Hospital ID:', detail[2])
+        print('Joining Date:', detail[3])
+        print('Speciality:', detail[4])
+        print('Salary:', detail[5])
+        print('Experience:', detail[6])
+    close_db(db)
 
 # getting doctors from hospital id
 def hospital_doctors(hospital_id):
@@ -71,8 +84,14 @@ def hospital_doctors(hospital_id):
     cursor.execute("""select * from doctor where hospital_id = %s""", (hospital_id,))
     d_list = cursor.fetchall()
     for detail in d_list:
-        print(detail[0], detail[1], detail[2], detail[3], detail[4], detail[5], detail[6])
-    close_db()
+        print('Doctor ID:', detail[0])
+        print('Doctor Name:', detail[1])
+        print('Hospital ID:', detail[2])
+        print('Joining Date:', detail[3])
+        print('Speciality:', detail[4])
+        print('Salary:', detail[5])
+        print('Experience:', detail[6])
+    close_db(db)
 
 # updating doctor experience
 def update_doctor_experience(experience, doctor_id):
@@ -80,4 +99,4 @@ def update_doctor_experience(experience, doctor_id):
     cursor = db.cursor()
     cursor.execute("""update doctor set experience = %s where doctor_id = %s """, (experience, doctor_id))
     db.commit()
-    close_db()
+    close_db(db)
